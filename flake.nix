177473cbef41e6148a5c504c7e7ee530bef974a2
inogai <inogai.sign@gmail.com>
2025-11-04 16:56:15 +0800

    chore: modify flake to use nixpkgs rust

--- flake.nix
+++ flake.nix
@@ -2,11 +2,7 @@
   description = "A Nix-flake-based Rust development environment";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
-    fenix = {
-      url = "https://flakehub.com/f/nix-community/fenix/0.1";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
   outputs =
@@ -26,37 +22,20 @@
           f {
             pkgs = import inputs.nixpkgs {
               inherit system;
-              overlays = [
-                inputs.self.overlays.default
-              ];
             };
           }
         );
     in
     {
-      overlays.default = final: prev: {
-        rustToolchain =
-          with inputs.fenix.packages.${prev.stdenv.hostPlatform.system};
-          combine (
-            with stable;
-            [
-              clippy
-              rustc
-              cargo
-              rustfmt
-              rust-src
-            ]
-          );
-      };
-
       devShells = forEachSupportedSystem (
         { pkgs }:
         {
           default = pkgs.mkShellNoCC {
             packages = with pkgs; [
-              rustToolchain
-              openssl
-              pkg-config
+              rustc
+              cargo
+              clippy
+              rustfmt
               cargo-deny
               cargo-edit
               cargo-watch
@@ -64,9 +43,20 @@
             ];
 
             env = {
-              # Required by rust-analyzer
-              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+              RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
             };
+          };
+        }
+      );
+
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "winterm-rs";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
           };
         }
       );
